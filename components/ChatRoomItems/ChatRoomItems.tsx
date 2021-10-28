55c8f2193239cbebee9a9faf9bab83935bdca0ce
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2021-10-28 15:27:57 +0530

    Adding Screen Not Found Code

--- components/ChatRoomItems/ChatRoomItems.tsx
+++ components/ChatRoomItems/ChatRoomItems.tsx
@@ -3,7 +3,7 @@ import { Image, StyleSheet, Text, View } from 'react-native'
 
 import styles from "./styles";
 
-export default function ChatRoom({ chatRoom }) {
+export default function ChatRoomItems({ chatRoom }) {
   const user = chatRoom.users[1];
   return (
     <View style={styles.container}>
@@ -20,9 +20,9 @@ export default function ChatRoom({ chatRoom }) {
         </View>
         <View style={styles.messageContainer}>
           <Text numberOfLines={1} style={styles.text}>{chatRoom.lastMessage.content}</Text>
-          <View style={styles.genderBadge}>
+          {user.gender && <View style={styles.genderBadge}>
             <Text style={styles.genderBadgeText}>{user.gender}</Text>
-          </View>
+          </View>}
         </View>
       </View>
     </View>
